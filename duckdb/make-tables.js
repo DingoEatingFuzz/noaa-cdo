53db18d478c827e8c68cba19c813d462c0e39953
Michael Lange <dingoeatingfuzz@gmail.com>
2022-06-25 19:03:54 -0700

    Alter the date column to be of type date.
    I think this could be done using a schema file during the csv2parquet
    step, but idk. Ideally the csv2parquet step gets skipped, it takes an
    absurd amount of time:
    
    Making the CSV:     20m
    Making the Parquet: 120m
    Making the DuckDB:  2m

--- duckdb/make-tables.js
+++ duckdb/make-tables.js
@@ -5,3 +5,5 @@ const con = db.connect();
 
 con.run(`CREATE TABLE IF NOT EXISTS noaa AS SELECT * FROM '../noaa-cdo.parquet'`);
 con.run(`CREATE TABLE IF NOT EXISTS stations AS SELECT * FROM '../noaa-stations.parquet'`);
+// Ideally this happens by default via the parquet import
+con.run(`ALTER TABLE noaa ALTER date TYPE DATE;`);
